59c4026bd7de94ddbe694d21f48aab8c5da50617
Joseph Ortiz <joseph.ortiz@cptc.edu>
2019-05-30 16:48:30 -0700

    Add Unit Tests for Course class properties to ensure values can still be set

--- BusinessLogicTests/CourseTests.cs
+++ BusinessLogicTests/CourseTests.cs
@@ -35,11 +35,31 @@ namespace BusinessLogicTests
         }
 
         [TestMethod]
+        [DataRow((byte)5)]
+        public void SetCreditsProperty_ValidCredits_SetsValue(byte numCredits)
+        {
+            //DO NOT MODIFY THIS TEST CODE. THE COURSE CLASS MAY NEED TO BE MODIFIED TO GET THE TEST TO PASS
+            Course c = new Course("CPW 215");
+            c.NumberOfCredits = numCredits;
+            Assert.AreEqual(c.NumberOfCredits, numCredits);
+        }
+
+        [TestMethod]
         public void SetCourseNameProperty_ToNull_ThrowsArgumentNullException()
         {
             //DO NOT MODIFY THIS TEST CODE. THE COURSE CLASS MUST BE MODIFIED TO GET THE TEST TO PASS
             Course c = new Course("CPW 215");
             Assert.ThrowsException<ArgumentNullException>(() => c.CourseName = null);
+        }
+
+        [TestMethod]
+        [DataRow("CPW 219")]
+        public void SetCourseNameProperty_ValidString_SetsCourseName(string courseName)
+        {
+            //DO NOT MODIFY THIS TEST CODE. THE COURSE CLASS MAY NEED TO BE MODIFIED TO GET THE TEST TO PASS
+            Course c = new Course("CPW 215");
+            c.CourseName = "CPW 219";
+            Assert.AreEqual(c.CourseName, courseName);
         }
     }
 }
